71ece8be633d7e5d80946919ded23bfff6d71eee
Pedro Ferreira <pfpm@protonmail.com>
2020-03-11 16:36:54 +0000

    Path monitor added.

--- PA1_P2G01/src/Monitors/MPath.java
+++ PA1_P2G01/src/Monitors/MPath.java
@@ -1,9 +1,10 @@
 package Monitors;
 
 import FarmInfrastructure.FIServer;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -14,44 +15,103 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class MPath {
     private final Integer movementTime;
+    private final Integer numFarmers;
+    private final Integer pathLength;
+    private Integer farmersWaiting;
     private final Integer numPositions;
     private Map<Integer, Integer[]> positions;
-    private ArrayList<Integer> order;
     private final FIServer fi;
     private final ReentrantLock rl;
+    private final Condition farmerProceed;
+    private final Condition farmerMoveForward;
     
     public MPath() {
         this.movementTime = 250;
+        this.numFarmers = 5;
+        this.pathLength = 10;
+        this.farmersWaiting = 0;
         this.numPositions = 5;
         this.positions = new HashMap<>();
-        this.order = new ArrayList<>();
         this.fi = new FIServer();
-        this.rl = new ReentrantLock();
+        this.rl = new ReentrantLock(true);
+        this.farmerProceed = rl.newCondition();
+        this.farmerMoveForward = rl.newCondition();
     }
     
     public MPath(FIServer fi) {
         this.movementTime = 250;
+        this.numFarmers = 5;
+        this.pathLength = 10;
+        this.farmersWaiting = 0;
         this.numPositions = 5;
         this.positions = new HashMap<>();
-        this.order = new ArrayList<>();
         this.fi = fi;
-        this.rl = new ReentrantLock();
+        this.rl = new ReentrantLock(true);
+        this.farmerProceed = rl.newCondition();
+        this.farmerMoveForward = rl.newCondition();
     }
     
     public MPath(FIServer fi, Integer movementTime, Integer numPositions) {
         this.movementTime = movementTime;
+        this.numFarmers = 5;
+        this.pathLength = 10;
+        this.farmersWaiting = 0;
         this.numPositions = numPositions;
         this.positions = new HashMap<>();
-        this.order = new ArrayList<>();
         this.fi = fi;
-        this.rl = new ReentrantLock();
+        this.rl = new ReentrantLock(true);
+        this.farmerProceed = rl.newCondition();
+        this.farmerMoveForward = rl.newCondition();
+    }
+    
+    public MPath(FIServer fi, 
+            Integer movementTime, 
+            Integer numPositions, 
+            Integer numFarmers) 
+    {
+        this.movementTime = movementTime;
+        this.numFarmers = numFarmers;
+        this.pathLength = 10;
+        this.farmersWaiting = 0;
+        this.numPositions = numPositions;
+        this.positions = new HashMap<>();
+        this.fi = fi;
+        this.rl = new ReentrantLock(true);
+        this.farmerProceed = rl.newCondition();
+        this.farmerMoveForward = rl.newCondition();
+    }
+    
+    public void enterPath(Integer id, boolean forward) {
+        rl.lock();
+        
+        try {
+            Integer[] position = this.getPosition(positions.get(id));
+            fi.movePath(id, position);
+            farmersWaiting++;
+            
+            while (!Objects.equals(farmersWaiting, numFarmers))
+                farmerProceed.await();
+            
+            if (Objects.equals(farmersWaiting, numFarmers)) {
+                farmersWaiting = 0;
+                farmerProceed.signalAll();
+            }
+        }
+        catch (InterruptedException e) {
+            System.err.println("ERROR: Farmer was badly interrupted when was "
+                    + "entering the Path!");
+        }
+        finally {
+            rl.unlock();
+        }
+        
     }
     
     public boolean moveForward(Integer id) {
         rl.lock();
         
         try {
-            Integer[] position = positions.get(id);
+            Integer[] position = this.getPosition(positions.get(id));
             Integer[] outsideCoordinates = {-1, -1};
 
             if (position != outsideCoordinates) {
@@ -59,16 +119,45 @@ public class MPath {
                 fi.movePath(id, position);
                 Thread.sleep(movementTime);
                 
-                if (!order.isEmpty()) {
-                    
-                }
+                while (rl.getHoldCount() != numFarmers)
+                    farmerMoveForward.await();
+                
+                if (rl.getHoldCount() == numFarmers)
+                    farmerMoveForward.signalAll();
+                
+                return false;
             }
+            
+            if (rl.getHoldCount() != 0)
+                farmerMoveForward.signal();
+            else
+                positions.clear();
+                
+            return true;
         }
         catch (InterruptedException e) {
-            System.err.println();
+            System.err.println("ERROR: Farmer was badly interrupted when "
+                    + "moving forward in the Path!");
         }
         finally {
             rl.unlock();
         }
+        
+        return false;
+    }
+    
+    private Integer[] getPosition(Integer[] prevPosition) {
+        Integer[] newPosition = prevPosition.clone();
+        
+        if (!Objects.equals(newPosition[0], pathLength)) {
+            newPosition[0] += 1;
+            newPosition[1] = (int)(Math.random() * (numFarmers - 1));
+        }
+        else {
+            newPosition[0] = -1;
+            newPosition[1] = -1;
+        }
+        
+        return newPosition;
     }
 }
