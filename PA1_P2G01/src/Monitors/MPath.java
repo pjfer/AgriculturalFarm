8da888c167b090c82821957d6fc6c26fe77bd0ff
Pedro Ferreira <pfpm@protonmail.com>
2020-03-14 13:55:23 +0000

    Changes/Fixes to the path monitor.

--- PA1_P2G01/src/Monitors/MPath.java
+++ PA1_P2G01/src/Monitors/MPath.java
@@ -1,9 +1,12 @@
 package Monitors;
 
 import FarmInfrastructure.FIController;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -14,34 +17,43 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author Pedro Ferreira and Rafael Teixeira
  */
 public class MPath {
+    private boolean toGranary;
     private Integer movementTime;
     private Integer numFarmers;
     private final Integer pathLength;
     private Integer numSteps;
     private Integer farmersWaiting;
+    private Queue<Integer> order;
     private Map<Integer, Integer[]> positions;
     private final FIController fiController;
     private final ReentrantLock rl;
     private final Condition farmerMoveForward;
     
     public MPath(FIController fiController) {
+        this.toGranary = true;
         this.movementTime = 250;
         this.numFarmers = 5;
         this.pathLength = 10;
         this.numSteps = 2;
         this.farmersWaiting = 0;
+        this.order = new LinkedList<>();
         this.positions = new HashMap<>();
         this.fiController = fiController;
         this.rl = new ReentrantLock(true);
         this.farmerMoveForward = rl.newCondition();
     }
     
-    public MPath(FIController fiController, Integer movementTime, Integer numPositions) {
+    public MPath(FIController fiController, 
+            Integer movementTime, 
+            Integer numPositions)
+    {
+        this.toGranary = true;
         this.movementTime = movementTime;
         this.numFarmers = 5;
         this.pathLength = 10;
         this.numSteps = 2;
         this.farmersWaiting = 0;
+        this.order = new LinkedList<>();
         this.positions = new HashMap<>();
         this.fiController = fiController;
         this.rl = new ReentrantLock(true);
@@ -53,34 +65,52 @@ public class MPath {
             Integer numFarmers,
             Integer numSteps) 
     {
+        this.toGranary = true;
         this.movementTime = movementTime;
         this.numFarmers = numFarmers;
         this.pathLength = 10;
         this.numSteps = numSteps;
         this.farmersWaiting = 0;
+        this.order = new LinkedList<>();
         this.positions = new HashMap<>();
         this.fiController = fiController;
         this.rl = new ReentrantLock(true);
         this.farmerMoveForward = rl.newCondition();
     }
     
-    public void enterPath(Integer id, boolean forward) {
+    public void prepareSimulation(int nf, int to) {
+        this.toGranary = true;
+        this.numFarmers = nf;
+        this.movementTime = to;
+        this.farmersWaiting = 0;
+        this.order = new LinkedList<>();
+        this.positions = new HashMap<>();
+    }
+    
+    public void enterPath(Integer id, boolean toGranary) {
         rl.lock();
         
         try {
-            Integer[] position = this.getPosition(new Integer[] {-1, -1});
+            this.toGranary = toGranary;
+            Integer[] position;
+            
+            if (toGranary)
+                position = this.getPositionToGranary(new Integer[] {-1, -1});
+            else
+                position = this.getPositionToStoreHouse(new Integer[] {-1, -1});
+            
             positions.put(id, position);
             fiController.movePath(id, position);
             farmersWaiting++;
-            
-            while (!Objects.equals(farmersWaiting, numFarmers))
-                farmerMoveForward.await();
             
             if (Objects.equals(farmersWaiting, numFarmers)) {
                 farmersWaiting = 0;
                 farmerMoveForward.signal();
                 farmerMoveForward.await();
             }
+            
+            while (!Objects.equals(farmersWaiting, 0))
+                farmerMoveForward.await();
         }
         catch (InterruptedException e) {
             System.err.println("ERROR: Farmer was badly interrupted when was "
@@ -96,25 +126,35 @@ public class MPath {
         rl.lock();
         
         try {
-            Integer[] position = this.getPosition(positions.get(id));
-            Integer[] outsideCoordinates = {-1, -1};
+            Integer[] position;
+            
+            if (toGranary)
+                position = this.getPositionToGranary(positions.get(id));
+            else
+                position = this.getPositionToStoreHouse(positions.get(id));
+            
+            Integer[] outsideCoordinates = { -1, -1 };
 
             if (position != outsideCoordinates) {
                 positions.put(id, position);
                 fiController.movePath(id, position);
                 Thread.sleep(movementTime);
+                farmerMoveForward.signal();
+                order.add(id);
                 
-                while (rl.getHoldCount() != numFarmers)
+                while (!Objects.equals(order.peek(), id) || order.size() == 1)
                     farmerMoveForward.await();
+                
+                order.remove();
                 
                 return false;
             }
             
-            if (rl.getHoldCount() != 0)
+            if (!order.isEmpty())
                 farmerMoveForward.signal();
             else
                 positions.clear();
-                
+
             return true;
         }
         catch (InterruptedException e) {
@@ -128,34 +168,64 @@ public class MPath {
         return false;
     }
     
-    private Integer[] getPosition(Integer[] prevPosition) {
+    private Integer[] getPositionToGranary(Integer[] prevPosition) {
         Integer[] newPosition = prevPosition.clone();
         
         if (newPosition[0] == -1 && newPosition[1] == -1) {
             newPosition[0] = 0;
             
             do {
-                newPosition[1] = (int)(Math.random() * (numFarmers - 1));
-            } while(positions.containsValue(newPosition));
-        }
-        else if (!Objects.equals(newPosition[0], pathLength)) {
-            do {
-                newPosition[0] += (int)(Math.random() * numSteps + 1);
-                newPosition[1] = (int)(Math.random() * (numFarmers - 1));
-            } while(positions.containsValue(newPosition));
+                newPosition[1] = (int)(Math.random() * numFarmers);
+            } while(positionTaken(newPosition));
         }
         else {
-            newPosition[0] = -1;
-            newPosition[1] = -1;
+            newPosition[0] = prevPosition[0] + 
+                    (int)(Math.random() * numSteps + 1);
+            
+            if (newPosition[0] <= pathLength - 1) {
+                do {
+                    newPosition[1] = (int)(Math.random() * numFarmers);
+                } while(positionTaken(newPosition));
+            }
+            else {
+                newPosition[0] = -1;
+                newPosition[1] = -1;
+            }
         }
         
         return newPosition;
     }
     
-    public void prepareSimulation(int nf, int to){
-        numFarmers = nf;
-        movementTime = to;
-        this.farmersWaiting = 0;
-        this.positions = new HashMap<>();
+    private Integer[] getPositionToStoreHouse(Integer[] prevPosition) {
+        Integer[] newPosition = prevPosition.clone();
+        
+        if (newPosition[0] == -1 && newPosition[1] == -1) {
+            newPosition[0] = pathLength - 1;
+            
+            do {
+                newPosition[1] = (int)(Math.random() * numFarmers);
+            } while(positionTaken(newPosition));
+        }
+        else {
+            newPosition[0] = prevPosition[0] - 
+                    (int)(Math.random() * numSteps + 1);
+            
+            if (newPosition[0] >= 0) {
+                do {
+                    newPosition[1] = (int)(Math.random() * numFarmers);
+                } while(positionTaken(newPosition));
+            }
+            else {
+                newPosition[0] = -1;
+                newPosition[1] = -1;
+            }
+        }
+        
+        return newPosition;
+    }
+    
+    private boolean positionTaken(Integer[] position) {
+        return positions.values().stream().anyMatch((posTaken) -> 
+                (Arrays.equals(position, posTaken)));
     }
 }
